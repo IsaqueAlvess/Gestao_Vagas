a95097c9526a95a4cbd858057ffd88f5d7b1a73d
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-05 22:43:32 -0300

    Creating roles for the candidate, and returning the candidate access_token

---
+++ src/main/java/br/com/isaque/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.isaque.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+    
+    public String access_token;
+}
