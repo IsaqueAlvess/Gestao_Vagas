11f4cb8beaf5d27d7573a5537fcab7b124d58557
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-19 22:24:04 -0300

    adding auth to swagger requests and fixing filter by excluding the caseSensitive from the Repository class using IgnoreCase

--- src/main/java/br/com/isaque/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/isaque/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,16 @@
 package br.com.isaque.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
     
+    @Schema(example = "Vaga p/ pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "Plano de Saúde, Odontológico, GymPass, EducaDev", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+    @Schema(example = "Júnior", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
--- src/main/java/br/com/isaque/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/isaque/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,10 +30,15 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Vaga de Design", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Plano de Saúde, Odontológico, EducaDev", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
 
     @NotBlank( message = "Esse campo é obrigatório")
+    @Schema(example = "Senior" , requiredMode = RequiredMode.NOT_REQUIRED)
     private String level;
 
     @ManyToOne()
