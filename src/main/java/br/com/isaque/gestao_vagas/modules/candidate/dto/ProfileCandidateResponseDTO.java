071b762af4df9e7b52fde8f3a25a66cf1c6d7f2b
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-20 00:38:38 -0300

    adding error returns to swagger examples

--- src/main/java/br/com/isaque/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/isaque/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.isaque.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +14,17 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProfileCandidateResponseDTO {
     
-    
+    @Schema(example = "Dev Java")
     private String description;
+
+    @Schema(example = "Maria_Silva")
     private String username;
+
+    @Schema(example = "maria@gmail.com")
     private String email;
+
+    @Schema(example = "Maria de Souza")
     private String name;
+    
     private UUID id;
 }
